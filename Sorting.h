f2b07128743e6e192916c086ea1eca38a41365f0
Renzo <renzo.tenazoa@utec.edu.pe>
2020-04-02 13:31:26 -0500

    feat(InsertSort): Added method InsertSort

---
+++ Sorting.h
@@ -0,0 +1,24 @@
+//
+// Created by Renzo on 1/04/2020.
+//
+
+#ifndef CS2100_SORTING_RENZOT_SORTING_H
+#define CS2100_SORTING_RENZOT_SORTING_H
+
+#include <vector>
+using namespace std;
+
+class Sorting {
+private:
+    vector<int> myvec;
+public:
+    Sorting(const vector<int> &myvec);
+    void imprimir();
+    //Mandatory
+    void InsertSort();
+    void SelectionSort();
+
+};
+
+
+#endif //CS2100_SORTING_RENZOT_SORTING_H
